7701290e092f039a767534ef98ec3458a63dc372
João Vitor <jvsantillo@gmail.com>
2017-10-28 17:42:47 -0200

    Atualizando projeto

--- xepa/App.js
+++ xepa/App.js
@@ -1,6 +1,6 @@
 //Importações
-import {AppRegistry, View, Image, Text, TouchableOpacity, Alert} from 'react-native';
-import React, {Component} from 'react';
+import { AppRegistry, View, Image, Text, TouchableOpacity, Alert } from 'react-native';
+import React, { Component } from 'react';
 
 //Formatações:
 
@@ -12,19 +12,19 @@ export const Estilo = {
     borderWidth: 2,
     borderRadius: 5,
     shadowColor: '#fff',
-    shadowOffset: {width:2, height:0},
-    shadowOpacity: 0.4, 
+    shadowOffset: { width: 2, height: 0 },
+    shadowOpacity: 0.4,
     marginTop: 20,
     marginHorizontal: 10,
     flexDirection: 'row',
-    paddingVertical: 10, 
+    paddingVertical: 10,
     paddingHorizontal: 30
-    
 
-  }, 
+
+  },
 
   botaoTexto: {
-    color: 'white' 
+    color: 'white'
   },
 
   texto: {
@@ -32,7 +32,7 @@ export const Estilo = {
     fontSize: 30,
     fontFamily: 'sans-serif',
     fontWeight: 'bold',
-    alignSelf: 'center', 
+    alignSelf: 'center',
     paddingTop: 15
 
   },
@@ -41,7 +41,7 @@ export const Estilo = {
     color: '#01161E',
     fontSize: 15,
     fontFamily: 'sans-serif',
-    alignSelf: 'center', 
+    alignSelf: 'center',
     paddingTop: 15,
     paddingHorizontal: 20
 
@@ -49,20 +49,20 @@ export const Estilo = {
 
   principal: {
     backgroundColor: '#D4D2D5',
-    flex: 1, 
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  }, 
+  },
 
   viewBotoes: {
     justifyContent: 'space-around',
-    alignItems: 'flex-start', 
+    alignItems: 'flex-start',
     flexDirection: 'column'
   },
 
   imagem: {
     paddingTop: 40,
-    width:400,
+    width: 400,
     height: 400
 
   },
@@ -76,61 +76,69 @@ export const Estilo = {
 };
 
 const botaoApertado = () => {
-  
-  var numeroAleatorio = Math.random()*4;
+
+  var numeroAleatorio = Math.random() * 4;
 
   numeroAleatorio = Math.round(numeroAleatorio);
 
-  var frases = ["Com a xepa, você vai ter controle sobre o seu dinheiro", "Economize já", "Em desenvolvimento", "Vai perder essa?", "Não há dia para economia!" ];
+  var frases = ["Com a xepa, você vai ter controle sobre o seu dinheiro", "Economize já", "Em desenvolvimento", "Vai perder essa?", "Não há dia para economia!"];
 
   Alert.alert(frases[numeroAleatorio]);
 };
 
-//Componente principal
+//Classe principal: ("Main")
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
-  render(){
+  render() {
     return (
-              <View style = {Estilo.principal}>
-                  <Image source = {{uri:'https://warpzone.me/loja/wp-content/uploads/2016/05/icon-ecommerce-app.png'}}
-                         style = {{width: 200, height: 200}}
-                  />  
-                  
-                  <Text style = {Estilo.texto}>Bem vindo a chepa! </Text>
-                  <Text style = {Estilo.textoMenor}>Selecione uma lista recente ou clique em "+" 
-                  para começar a aeconomizar </Text>
-  
-                  
-                  <View style = {Estilo.viewBotoes}> 
-                        <TouchableOpacity onPress = {botaoApertado} 
-                                    style = {Estilo.botao}
-                                    
-                        >
-                                <Text style = {Estilo.botaoTexto}>Lista 1</Text>
-  
-                        </TouchableOpacity>
-  
-                        <TouchableOpacity onPress = {botaoApertado} 
-                                    style = {Estilo.botao}        
-                        >
-                                <Text style = {Estilo.botaoTexto}>Lista 2</Text>   
-                        </TouchableOpacity>
-                  </View>
-                  <View style = {Estilo.viewBotoes}>
-                    <TouchableOpacity onPress = {botaoApertado} 
-                                      style = {Estilo.botaoAdd}
-                                    
-                    >
-                          <Image source = {{uri:'https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/add-128.png'}}
-                                 style = {{width: 30, height: 30}}                        
-                          />
-  
-                    </TouchableOpacity>
-                  </View>  
-              </View>
+      <View style={Estilo.principal}>
+        <ImagemPrincipal></ImagemPrincipal>
+        <TextoBoasVindas></TextoBoasVindas>
+        <Botao></Botao>
+      </View>
     );
   }
 };
+
+export class ImagemPrincipal extends Component {
+  render() {
+    return (
+      <Image source={{ uri: 'https://warpzone.me/loja/wp-content/uploads/2016/05/icon-ecommerce-app.png' }}
+        style={{ width: 200, height: 200 }}
+      />
+    );
+  }
+};
+
+export class TextoBoasVindas extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={Estilo.texto}>Bem vindo a xepa! </Text>
+      </View>
+    );
+  }
+};
+
+export class Botao extends Component {
+  render() {
+    return (
+      <View> 
+            <View style={Estilo.viewBotoes}>
+              <TouchableOpacity onPress={botaoApertado}
+                style={Estilo.botao}
+              >
+                <Text style={Estilo.botaoTexto}>Comece a economizar já!</Text>
+              </TouchableOpacity>
+            </View>
+      </View>
+    );
+  }
+};
+
+
+
+
 
