a10070eaea3482dcde4c5ff70b91f0b48672334a
Natália Marufuji Aoki <nataliamarufuji@gmail.com>
2017-12-07 00:15:35 -0200

    ajustes design tela bem vinda

--- xepa/src/components/ViewInicial.js
+++ xepa/src/components/ViewInicial.js
@@ -23,24 +23,17 @@ export default class ViewInicial extends Component {
   render() {
     const pageArray = [{
       title: 'Bem-vindo ao xepa!',
-      description: 'Aqui é economia de VERDADE!',
+      description: 'Se organize de maneira mais fácil e mais prática para suas compras',
       img: require('xepa/resources/img/Ativo5.png'),
       imgStyle: {
-        height: 50 * 2.5,
         width: 130 * 2.5,
       },
-      backgroundColor: '#fa931d',
-      fontColor: '#fff',
       level: 10,
     }, {
       title: 'Comece a utilizar o app já e veja suas economias durarem mais!',
       description: 'Controle a sua dispensa, suas listas de compras e seu orçamento com o Xepa!',
-      img: require('xepa/resources/img/iconeTeste.png'),
       imgStyle: {
-        height: 100 * 2.5,
-        width: 103 * 2.5,
       },
-      backgroundColor: '#a4b602',
       fontColor: '#fff',
       level: 10,
     }];
